d9ca22c1e7bd1f82c88e9b6a6e708bcc983dd802
TennisStudent <73370352+TennisStudent@users.noreply.github.com>
2021-08-01 20:19:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300)
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.hide();
+}
+
+function draw()
+{
+
+}
+function save_image()
+{
+
+}
